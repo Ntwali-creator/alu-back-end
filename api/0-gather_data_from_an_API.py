4e2ce344f365abe961c5f5d23e094c3ea4a7c195
James-Ntwali <j.ntwali@alustudent.com>
2026-07-21 10:05:36 +0200

    james
    Refactored the script to simplify fetching and displaying employee TODO list progress using a REST API. Removed the previous function and integrated the logic directly into the main section.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,47 +1,27 @@
 #!/usr/bin/python3
 """
-This script fetches and displays TODO list progress for a given employee ID.
+Using a REST API, and a given emp_ID, return info about their TODO list.
 """
-
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Fetch and display employee TODO list progress.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    user_response = requests.get(base_url + "/users/{}".format(employee_id))
-    if user_response.status_code != 200:
-        return
-
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
-
-    todos_response = requests.get(base_url + "/todos", params={"userId": employee_id})
-    if todos_response.status_code != 200:
-        return
-
-    todos = todos_response.json()
-
-    total_tasks = len(todos)
-    done_tasks = [todo for todo in todos if todo.get("completed")]
-    number_of_done_tasks = len(done_tasks)
-
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, number_of_done_tasks, total_tasks))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(1)
+    """ main section """
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    employee = requests.get(
+        BASE_URL + f'/users/{sys.argv[1]}/').json()
+    EMPLOYEE_NAME = employee.get("name")
+    employee_todos = requests.get(
+        BASE_URL + f'/users/{sys.argv[1]}/todos').json()
+    serialized_todos = {}
 
-    try:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
-    except ValueError:
-        sys.exit(1)
+    for todo in employee_todos:
+        serialized_todos.update({todo.get("title"): todo.get("completed")})
+
+    COMPLETED_LEN = len([k for k, v in serialized_todos.items() if v is True])
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, COMPLETED_LEN, len(serialized_todos)))
+    for key, val in serialized_todos.items():
+        if val is True:
+            print("\t {}".format(key))
