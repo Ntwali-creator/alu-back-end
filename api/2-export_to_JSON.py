b50bbb18b0281b51288e643c57a5378b936b7d75
James-Ntwali <j.ntwali@alustudent.com>
2026-07-21 10:07:16 +0200

    james

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,69 +1,35 @@
 #!/usr/bin/python3
 """
-This script exports employee TODO list data to JSON format.
+Using a REST API and an EMP_ID, save info about their TODO list in a json file
 """
-
 import json
 import requests
 import sys
 
+if __name__ == "__main__":
+    """ Main section """
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1] if len(sys.argv) > 1 else None
 
-def export_todos_to_json(employee_id):
-    """
-    Export employee todos to JSON file.
-    
-    Args:
-        employee_id (int): The ID of the employee
-        
-    Returns:
-        None
-    """
-    # Base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
-    
-    # Fetch user data
-    user_response = requests.get(base_url + "/users/{}".format(employee_id))
-    if user_response.status_code != 200:
-        print("Employee not found")
-        return
-    
-    user_data = user_response.json()
-    username = user_data.get("username")
-    
-    # Fetch todos for the user
-    todos_response = requests.get(base_url + "/todos", params={"userId": employee_id})
-    if todos_response.status_code != 200:
-        print("Could not fetch todos")
-        return
-    
-    todos = todos_response.json()
-    
-    # Prepare data structure
-    tasks_list = []
-    for todo in todos:
-        task_dict = {
+    if not employee_id:
+        print("Please provide an employee ID as an argument.")
+        sys.exit(1)
+
+    employee = requests.get(f"{BASE_URL}/users/{employee_id}/").json()
+    employee_name = employee.get("username")
+    emp_todos = requests.get(f"{BASE_URL}/users/{employee_id}/todos").json()
+    serialized_todos = []
+
+    for todo in emp_todos:
+        serialized_todos.append({
             "task": todo.get("title"),
             "completed": todo.get("completed"),
-            "username": username
-        }
-        tasks_list.append(task_dict)
-    
-    data = {str(employee_id): tasks_list}
-    
-    # Write to JSON file
-    filename = "{}.json".format(employee_id)
-    with open(filename, mode='w', encoding='utf-8') as json_file:
-        json.dump(data, json_file)
+            "username": employee_name
+        })
 
+    output_data = {employee_id: serialized_todos}
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
-        sys.exit(1)
-    
-    try:
-        employee_id = int(sys.argv[1])
-        export_todos_to_json(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
+    with open(f"{employee_id}.json", 'w') as file:
+        json.dump(output_data, file, indent=4)
+
+    print(f"Tasks for employee {employee_id} exported to {file_name}.")
