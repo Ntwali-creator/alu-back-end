59178979752d24d46c16edbe57d04b01c287e9a3
Ntwali-creator <j.ntwali@alustudent.com>
2026-07-21 02:10:48 -0700

    Merge remote master branch

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,27 +1,53 @@
 #!/usr/bin/python3
+
 """
-Using a REST API, and a given emp_ID, return info about their TODO list.
+This module fetches and displays TODO list progress for a given employee ID.
 """
+
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    """ main section """
-    BASE_URL = 'https://jsonplaceholder.typicode.com'
-    employee = requests.get(
-        BASE_URL + f'/users/{sys.argv[1]}/').json()
-    EMPLOYEE_NAME = employee.get("name")
-    employee_todos = requests.get(
-        BASE_URL + f'/users/{sys.argv[1]}/todos').json()
-    serialized_todos = {}
+def get_employee_todo_progress(employee_id):
+    """
+    Retrieves and displays TODO list progress for a specific employee.
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    
+    # Fetch user information
 
-    for todo in employee_todos:
-        serialized_todos.update({todo.get("title"): todo.get("completed")})
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
+    if user_response.status_code != 200:
+        return
+    
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
+    
+    # Fetch TODO list
 
-    COMPLETED_LEN = len([k for k, v in serialized_todos.items() if v is True])
+    todos_response = requests.get(f"{base_url}/todos", params={"userId": employee_id})
+    if todos_response.status_code != 200:
+        return
+    
+    todos_data = todos_response.json()
+    
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task.get("completed")]
+    number_done = len(completed_tasks)
+    
     print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, COMPLETED_LEN, len(serialized_todos)))
-    for key, val in serialized_todos.items():
-        if val is True:
-            print("\t {}".format(key))
+        employee_name, number_done, total_tasks))
+    
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    
+    try:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
+    except ValueError:
+        sys.exit(1)
